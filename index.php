c0ebb09e0e5036bfcecc35a7389b94d132efb09c
Gavin Sherman <gavinmsherman@gmail.com>
2022-04-14 13:28:21 -0400

    changed index to render from views

--- index.php
+++ index.php
@@ -13,10 +13,10 @@ $f3 = Base::instance();
 //Define a default route
 $f3->route('GET /', function() {
 
-    echo"<h1>Pets Home</h1>";
+    //echo"<h1>Pets Home</h1>";
 
-    //$view = new Template();
-    //echo $view->render('views/home.html');
+    $view = new Template();
+    echo $view->render('views/home.html');
 }
 );
 
